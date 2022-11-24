bcfe0b0214400907c64401b5c361443a758a84b8
NatigAgarzayev <116652802+NatigAgarzayev@users.noreply.github.com>
2022-11-24 19:27:11 +0400

    Add files via upload

---
+++ task/js/script.js
@@ -0,0 +1,11 @@
+let nameArr = ['resad', 'raul', 'timur', 'lale', 'kerim'];
+let lastNameArr = ['ibrahimov(-a)', 'Mammadov(-a)', 'Rasulov(-a)', 'Xeyyam'];
+
+
+const name = document.querySelector('.input__name'),
+    lastName = document.querySelector('.input__lastname'),
+    date = document.querySelector('.input__date'),
+    mail = document.querySelector('.input__email'),
+    number = document.querySelector('.input__number'),
+    gender = document.querySelectorAll('.gender > input'),
+    btn = document.querySelector('.btn');
